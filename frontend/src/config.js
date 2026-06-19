9e57207898fac836f0e519a63fe23f3f652838d9
osiasnalaunan68-star <osiasnalaunan68@gmail.com>
2026-06-19 15:43:10 +0800

    UI: apply portfolio-style dark/gold theme, update date to June 19, 2026, add About link to customs-docs

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL = "https://customs-ph-webservice.onrender.com";
 export const TARIFF_VERSION = "AHTN 2022";
-export const LAST_UPDATED = "June 2024";
+export const LAST_UPDATED = "June 19, 2026";
 export const CURRENCYFREAKS_KEY = "a43ca6ae21a7489f8b4edd53a1343e00";
